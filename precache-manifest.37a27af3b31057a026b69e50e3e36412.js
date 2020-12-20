d932b05e456d090a53bcf0daffec9aeb59cb2a97
Peter Trofimov <ptrofimov7@gmail.com>
2020-12-20 13:15:36 +0200

    Updates

---
+++ precache-manifest.37a27af3b31057a026b69e50e3e36412.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a91ad6154f29f5e4001262ce51315a",
+    "url": "/React-CheckList-App/index.html"
+  },
+  {
+    "revision": "30e1b64779426d1613d1",
+    "url": "/React-CheckList-App/static/css/main.ba12695f.chunk.css"
+  },
+  {
+    "revision": "f55c8831888384d42ac9",
+    "url": "/React-CheckList-App/static/js/2.af89d38a.chunk.js"
+  },
+  {
+    "revision": "30e1b64779426d1613d1",
+    "url": "/React-CheckList-App/static/js/main.dcf0dc06.chunk.js"
+  },
+  {
+    "revision": "4d2a7e560a75ac779127",
+    "url": "/React-CheckList-App/static/js/runtime~main.3fb5d5fd.js"
+  }
+]);
